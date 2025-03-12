e9d3b31cdd173fcbda6f4893d6be8705b8d98d6b
Miguel Domingo <jdomi8@illinois.edu>
2025-03-12 00:47:45 -0500

    Initial Commit

---
+++ CameraWebSocketV1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraWebSocketV1
+//
+//  Created by Miguel Domingo on 3/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
